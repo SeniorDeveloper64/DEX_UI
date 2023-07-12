30db9de83794c7039c8f58b1902361d39c35bae0
SeniorDeveloper64 <seniorsoftdeveloper64@gmail.com>
2023-07-12 10:32:31 -0400

    fixed select option position

--- src/pages/LaunchApp/ETHUSDSwap.jsx
+++ src/pages/LaunchApp/ETHUSDSwap.jsx
@@ -6,17 +6,14 @@ import WETHABI from "../../constants/abi/WethABI.json";
 import bigInt from "big-integer";
 
 const ETHUSDSwapSection = () => {
-  const web3 = new Web3(window.ethereum);
-
   const [selectedInput, setSelectedInput] = useState("eth");
   const [selectedOutput, setSelectedOutput] = useState("usdt");
   const [inputAmount, setInputAmount] = useState("");
   const [outputAmount, setOutputAmount] = useState("");
   const [gasFee, setGasFee] = useState("");
-
-  console.log(outputAmount);
-
   const [account, setAccount] = useState("");
+
+  const web3 = new Web3(window.ethereum);
 
   useEffect(() => {
     getAccount();
@@ -26,6 +23,10 @@ const ETHUSDSwapSection = () => {
     handleOutputAmount(inputAmount);
   }, [inputAmount]);
 
+  useEffect(() => {
+    handleInputAmount(outputAmount);
+  }, [outputAmount]);
+
   const getAccount = async () => {
     try {
       const accounts = await web3.eth.getAccounts();
@@ -34,37 +35,54 @@ const ETHUSDSwapSection = () => {
     } catch (error) {}
   };
 
+  const handleInputAmount = async (outputAmount) => {
+    console.log("handleInputAmount");
+  };
+
   const handleOutputAmount = async (inputAmount) => {
     if (inputAmount == 0) {
-      setOutputAmount(0);
-      setGasFee(0);
+      setOutputAmount("");
+      setGasFee("");
     } else {
       const uniswapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
       const uniswapRouterABI = UniswapV2Router02ABI;
-
       const uniswapRouterContract = new web3.eth.Contract(
         uniswapRouterABI,
         uniswapRouterAddress
       );
+
       const wethAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
-      const tokenAddress = "0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49"; // TODO: Replace with output token address
+      const tokenAddress = "0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49";
 
-      const amounts = await uniswapRouterContract.methods
-        .getAmountsOut(web3.utils.toWei(inputAmount, "ether"), [
-          wethAddress,
-          tokenAddress,
-        ])
-        .call();
+      if (selectedInput == "eth") {
+        const amounts = await uniswapRouterContract.methods
+          .getAmountsOut(web3.utils.toWei(inputAmount, "ether"), [
+            wethAddress,
+            tokenAddress,
+          ])
+          .call();
 
-      console.log(amounts);
-      const outputAmount = bigInt(amounts[1]) / 1e6;
-      setOutputAmount(outputAmount);
+        console.log(amounts);
+        const outputAmount = bigInt(amounts[1]) / 1e6;
+        setOutputAmount(outputAmount);
+        // console.log(Date.now() + 1000 * 60 * 3);
+      } else {
+        const amounts = await uniswapRouterContract.methods
+          .getAmountsOut(
+            bigInt(bigInt(Math.floor(inputAmount)) * 1e6).toString(),
+            [tokenAddress, wethAddress]
+          )
+          .call();
+
+        console.log(amounts);
+        const outputAmount = bigInt(amounts[1]) / 1e18;
+        setOutputAmount(outputAmount);
+      }
 
       const gasPrice = await web3.eth.getGasPrice();
       const gasFee = (bigInt(gasPrice) * bigInt(1000000)) / bigInt(1e18);
       setGasFee(gasFee);
-
-      console.log(Date.now() + 1000 * 60 * 3);
+      // console.log(Date.now() + 1000 * 60 * 3);
     }
   };
 
@@ -96,45 +114,71 @@ const ETHUSDSwapSection = () => {
       const TokenABI = IERC20TokenABI;
       const TokenContract = new web3.eth.Contract(TokenABI, tokenAddress);
 
-      const amounts = await uniswapRouterContract.methods
-        .getAmountsOut(web3.utils.toWei(inputAmount, "ether"), [
-          wethAddress,
-          tokenAddress,
-        ])
-        .call();
+      if (selectedInput == "eth") {
+        const amounts = await uniswapRouterContract.methods
+          .getAmountsOut(web3.utils.toWei(inputAmount, "ether"), [
+            wethAddress,
+            tokenAddress,
+          ])
+          .call();
 
-      await wethContract.methods
-        .deposit()
-        .send({ from: account, value: amounts[0] });
+        console.log(amounts);
+        const inputAmountWei = amounts[0];
+        const outputAmountWei = amounts[1];
 
-      const inputAmountWei = amounts[0];
+        await wethContract.methods
+          .deposit()
+          .send({ from: account, value: amounts[0] });
 
-      console.log(amounts);
-      const outputAmountWei = amounts[1];
-      console.log(outputAmountWei);
-      console.log("account");
-      console.log(account);
+        await TokenContract.methods
+          .approve(uniswapRouterAddress, outputAmountWei)
+          .send({ from: account });
 
-      await TokenContract.methods
-        .approve(uniswapRouterAddress, outputAmountWei)
-        .send({ from: account });
+        await uniswapRouterContract.methods
+          .swapExactETHForTokens(
+            outputAmountWei,
+            [wethAddress, tokenAddress],
+            account,
+            Date.now() + 1000 * 60 * 3
+          )
+          .send({ from: account, value: inputAmountWei });
 
-      await uniswapRouterContract.methods
-        .swapExactETHForTokens(
-          outputAmountWei,
-          [wethAddress, tokenAddress],
-          account,
-          Date.now() + 1000 * 60 * 3
-        )
-        .send({ from: account, value: inputAmountWei });
+        await wethContract.methods
+          .withdraw(web3.utils.toWei(inputAmount, "ether"))
+          .send({ from: account });
+      } else {
+        const amounts = await uniswapRouterContract.methods
+          .getAmountsOut(
+            bigInt(bigInt(Math.floor(inputAmount)) * 1e6).toString(),
+            [tokenAddress, wethAddress]
+          )
+          .call();
 
-      // Unwrap WETH into ETH
-      await wethContract.methods
-        .withdraw(web3.utils.toWei(inputAmount, "ether"))
-        .send({ from: account });
+        console.log(amounts);
+        const inputAmountWei = amounts[0];
+        const outputAmountWei = amounts[1];
 
-      // Update the output amount in the UI
-      // setOutputAmount(outputAmount);
+        await TokenContract.methods
+          .approve(uniswapRouterAddress, inputAmountWei)
+          .send({ from: account });
+
+        // await wethContract.methods
+        //   .deposit()
+        //   .send({ from: account, value: outputAmountWei });
+
+        await uniswapRouterContract.methods
+          .swapExactTokensForETH(
+            inputAmountWei,
+            [tokenAddress, wethAddress],
+            account,
+            Date.now() + 1000 * 60 * 3
+          )
+          .send({ from: account, value: outputAmountWei });
+
+        await wethContract.methods
+          .withdraw(web3.utils.toWei(outputAmount, "ether"))
+          .send({ from: account });
+      }
     } catch (error) {
       console.error(error);
     }
@@ -144,10 +188,24 @@ const ETHUSDSwapSection = () => {
     <div>
       <div className="contact-form-wrap-launch">
         <div className="row">
-          <div className="col-md-2">
-            <select value={selectedInput} onChange={handleInputChange}>
-              <option value="eth">ETH</option>
-              <option value="usdt">USDT</option>
+          <div className="col-md-2" style={{ padding: 20 }}>
+            <select
+              value={selectedInput}
+              style={{
+                backgroundColor:
+                  selectedInput === "eth" ? "#0b1d33" : "#1d2024",
+                color: "white",
+                borderRadius: "10px",
+                border: "none",
+              }}
+              onChange={handleInputChange}
+            >
+              <option value="eth" style={{ backgroundColor: "blue" }}>
+                ETH
+              </option>
+              <option value="usdt" style={{ backgroundColor: "grey" }}>
+                USDT
+              </option>
             </select>
           </div>
 
@@ -163,24 +221,40 @@ const ETHUSDSwapSection = () => {
             </div>
           </div>
 
-          <div className="col-md-2">
-            <select value={selectedOutput} onChange={handleOutputChange}>
-              <option value="eth">ETH</option>
-              <option value="usdt">USDT</option>
+          <div className="col-md-2" style={{ padding: 20 }}>
+            <select
+              value={selectedOutput}
+              style={{
+                backgroundColor:
+                  selectedOutput === "eth" ? "#0b1d33" : "#1d2024",
+                color: "white",
+                borderRadius: "10px",
+                border: "none",
+              }}
+              onChange={handleOutputChange}
+            >
+              <option value="eth" style={{ backgroundColor: "blue" }}>
+                ETH
+              </option>
+              <option value="usdt" style={{ backgroundColor: "grey" }}>
+                USDT
+              </option>
             </select>
           </div>
           <div className="col-md-10">
             <div className="form-grp">
               <input
                 type="number"
+                min="0"
                 id="outputAmount"
                 value={outputAmount}
+                onChange={(e) => setOutputAmount(e.target.value)}
                 readOnly
               />
             </div>
           </div>
 
-          <div className="col-md-2">
+          <div className="col-md-2" style={{ padding: 20 }}>
             <span>FEE: </span>
           </div>
           <div className="col-md-10">
